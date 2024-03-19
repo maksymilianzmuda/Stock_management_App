e56ab3ab7d40b6354f98dc3a1d20247648b5582a
maksymilianzmuda <maksymilianzmuda02@gmail.com>
2024-03-19 19:58:43 +0100

    first commit

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId 
+            };
+        }
+
+         public static Comment ToCommentFromCreate(this CreateCommentRequestDto commentModel,int stockId)
+        {
+            return new Comment
+            {
+                
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                StockId = stockId
+                
+            };
+        }
+
+         public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto commentModel)
+        {
+            return new Comment
+            {
+                
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                
+                
+            };
+        }
+    }
+}
