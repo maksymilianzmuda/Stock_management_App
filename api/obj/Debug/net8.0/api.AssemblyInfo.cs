cb30cde7b8deac2bf4daa73c93ed704e1cd546c1
maksymilianzmuda <maksymilianzmuda02@gmail.com>
2024-04-22 22:53:39 +0200

    migration

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6fd8ee490d6b8020e1e928ac39b099b724f187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e430f63eb1f60c9173cbd91b8ce4f3a23eb8e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
